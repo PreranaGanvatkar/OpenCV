0e49934f772580f982aee71ad0442d92c6115784
PreranaGanvatkar <preranaganvatkar@gmail.com>
2021-11-28 16:34:16 +0530

    updated

---
+++ Basics.py
@@ -0,0 +1,17 @@
+import cv2,time
+
+video=cv2.VideoCapture(0)
+a=1
+while True:
+    a=a+1
+    check,frame=video.read()
+    print(frame)
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('Capturing',frame)
+    Key=cv2.waitKey(1)
+    if Key==ord('q'):
+        break
+
+print(a)
+video.release()
+cv2.destroyAllWindows()
